0099ba68cdd7b6480848ded49b3645584cb076eb
Roarcannotprogramming <paulzh@mail.ustc.edu.cn>
2018-06-01 21:55:08 +0800

    change the index.html use of css Updated by zh_roar

--- user/admin.py
+++ user/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.db import models
 from django import forms
-from .models import common_member,common_member_star
+from .models import common_member, common_member_star
 
 
-class Common_memberAdmin(admin.ModelAdmin):
-    list_display = ('uid','username','status')
-
-
-class Commmon_menber_starAdmin(admin.ModelAdmin):
-    list_display = ('uid','is_school_info','pid')
-
-
-
-admin.site.register(common_member, Common_memberAdmin)
-admin.site.register(common_member_star, Commmon_menber_starAdmin)
+# class Common_memberAdmin(admin.ModelAdmin):
+#     list_display = ('uid','username','status')
+#
+#
+# class Commmon_menber_starAdmin(admin.ModelAdmin):
+#     list_display = ('uid', 'is_school_info', 'pid')
+#
+#
+#
+# admin.site.register(common_member, Common_memberAdmin)
+#
+# admin.site.register(common_member_star, Commmon_menber_starAdmin)
