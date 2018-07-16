522989080c0200f6a1f1e5dd63f198b3195505f7
Crescentweiss <36957992+Crescentweiss@users.noreply.github.com>
2018-07-16 23:46:28 +0800

    修复滚动条定位不准确的问题

--- BBS_model/js/origional.js
+++ BBS_model/js/origional.js
@@ -1,8 +1,12 @@
 function overShow(obj,e) {
     var showDiv = document.getElementById('showDiv');
     var theEvent = window.event|| e;
-    showDiv.style.left = theEvent.clientX+"px";
-    showDiv.style.top = theEvent.clientY+"px";
+    var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+    var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+    var x = e.pageX || e.clientX + scrollX;
+    var y = e.pageY || e.clientY + scrollY;
+    showDiv.style.left = x+'px';
+    showDiv.style.top = y+'px';
     showDiv.style.display = 'block';
     //alert(obj.innerHTML);
     showDiv.innerHTML = obj.innerHTML;
@@ -13,3 +17,5 @@ function outHide() {
     showDiv.style.display = 'none';
     showDiv.innerHTML = '';
 }
+
+
