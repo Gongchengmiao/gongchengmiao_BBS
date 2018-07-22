b0e4694819360c39afa4a2fa93406a92990e1662
Zhang Hao <paulzh@mail.ustc.edu.cn>
2018-07-22 21:31:02 +0800

    new user

--- chat/consumers.py
+++ chat/consumers.py
@@ -76,6 +76,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.user = self.scope['user']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
@@ -94,7 +95,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
@@ -103,15 +104,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user': self.user.username,
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        user = event['user']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'user': user,
         }))
