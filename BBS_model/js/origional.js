1bbdb21349fa351b697e0d6b93a6fd5b639a8991
Crescentweiss <36957992+Crescentweiss@users.noreply.github.com>
2018-07-22 22:05:50 +0800

    新增聊天界面的js

--- BBS_model/js/origional.js
+++ BBS_model/js/origional.js
@@ -73,3 +73,63 @@ function loadData()
     document.getElementById("update").innerHTML=xmlhttp.responseText;
 }
 
+
+
+
+function talk() {
+
+    var Words = document.getElementById("words");
+    var TalkWords = document.getElementById("talkwords");
+    var TalkSub = document.getElementById("talksub");
+
+    TalkSub.onclick = function(){
+        //定义空字符串
+        var str = "";
+        if(TalkWords.value == ""){
+            // 消息为空时弹窗
+            alert("消息不能为空");
+            return;
+        }
+        str = '<div class="metalk">' +
+              '<span>' +
+              '<a href="x_personal_page_show_demo.html" target="_blank" style="color: #0d8ddb">A</a>' + ":" + TalkWords.value +
+              '</span>' +
+              '</div>';
+        Words.innerHTML = Words.innerHTML + str;
+        Words.scrollTop = Words.scrollHeight;
+    };
+}
+
+
+function Recieve() {
+    var Words = document.getElementById("words");
+    var str = "";
+    var str2 = str;
+
+    function getJson()
+    {
+        $.getJSON("js/userinfo.json", function (data) {
+            $.each(data, function (infoIndex, info) {
+                str = '<div class="othertalk">' +
+                    '<span>' +
+                    '<a href="x_personal_page_show_demo.html" target="_blank" style="color: #0d8ddb">info["name"]</a>' + ":" + info["message"] +
+                    '</span>' +
+                    '</div>';
+
+            });
+        });
+    }
+
+    if(str2 === str){
+        setTimeout("getJson()",1000);
+    }
+    else {
+        str2 = str;
+    }
+
+    if (str != "") {
+        Words.innerHTML = Words.innerHTML + str;
+        Words.scrollTop = Words.scrollHeight;
+    }
+    setTimeout("Recieve()",1000);
+}
